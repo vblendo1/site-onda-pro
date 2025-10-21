6638d8f0b44710165acd06f1dc1417e41f11decb
Victor Blendo <victorblendooficial@gmail.com>
2025-10-21 13:04:46 -0300

    Added index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Navbar2 } from "./components/Navbar2";
+import { Header86 } from "./components/Header86";
+import { Testimonial13 } from "./components/Testimonial13";
+import { Layout357 } from "./components/Layout357";
+import { Product10 } from "./components/Product10";
+import { Testimonial22 } from "./components/Testimonial22";
+import { Blog44 } from "./components/Blog44";
+import { Contact6 } from "./components/Contact6";
+import { Layout1 } from "./components/Layout1";
+import { Footer3 } from "./components/Footer3";
+
+export default function Page() {
+  return (
+    <div>
+      <Navbar2 />
+      <Header86 />
+      <Testimonial13 />
+      <Layout357 />
+      <Product10 />
+      <Testimonial22 />
+      <Blog44 />
+      <Contact6 />
+      <Layout1 />
+      <Footer3 />
+    </div>
+  );
+}
